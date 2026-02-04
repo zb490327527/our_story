bae63ee1da8085dff362b3853461a45bfbb00b75
JYXC-DZ-0100157 <jyxc-dz-0100157@JYXC-DZ-0100157deMacBook-Pro.local>
2026-02-04 17:32:02 +0800

    final: standard config cleanup

--- next.config.ts
+++ next.config.ts
@@ -6,13 +6,6 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "picsum.photos", pathname: "/**" },
     ],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  // @ts-expect-error - eslint config is valid
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
